893c715d75a9f40a7706822a0af04ead52d63ee1
Max Levitskiy <max@webtree.org>
2020-09-17 20:32:20 +0200

    Configuration added

---
+++ back/api/conf/config.go
@@ -0,0 +1,77 @@
+package conf
+
+import (
+	"fmt"
+	"github.com/kelseyhightower/envconfig"
+	"gopkg.in/yaml.v2"
+	"log"
+	"os"
+	"strings"
+)
+
+//
+type config struct {
+	Elastic struct {
+		Server string `yaml:"server" envconfig:"ELASTIC_SERVER"`
+		Port   int    `yaml:"port" envconfig:"ELASTIC_PORT"`
+		Index  struct {
+			Resource struct {
+				Name string `yaml:"name" envconfig:"ELASTIC_INDEX_RESOURCE_NAME"`
+			} `yaml:"resource"`
+		} `yaml:"index"`
+	} `yaml:"elastic"`
+}
+
+var Inst = initConfig()
+
+func initConfig() config {
+	var cfg = config{}
+	confPostfix := os.Getenv("CONFIG_FILE_POSTFIX")
+	readFile(&cfg, confPostfix)
+	readEnv(&cfg)
+	return cfg
+}
+
+func AddConfigs(filesPostfixes ...string) {
+	for _, postfix := range filesPostfixes {
+		readFile(&Inst, postfix)
+	}
+	readEnv(&Inst)
+}
+
+func Reset() {
+	Inst = initConfig()
+}
+
+func readFile(cfg *config, filePostfix string) {
+	filePostfix = strings.TrimSpace(filePostfix)
+	var fileName string
+	if filePostfix == "" {
+		fileName = "config.yaml"
+	} else {
+		fileName = fmt.Sprintf("config_%s.yaml", filePostfix)
+	}
+
+	f, err := os.Open("conf/" + fileName)
+	if err != nil {
+		processError(err)
+	}
+	defer f.Close()
+
+	decoder := yaml.NewDecoder(f)
+	err = decoder.Decode(cfg)
+	if err != nil {
+		processError(err)
+	}
+}
+
+func readEnv(cfg *config) {
+	err := envconfig.Process("", cfg)
+	if err != nil {
+		processError(err)
+	}
+}
+func processError(err error) {
+	fmt.Println(err)
+	log.Fatal("Can't read config. Exit.")
+}
