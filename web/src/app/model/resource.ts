2789794c52718da526c6faec1a35df441438532e
Max Levitskiy <max@webtree.org>
2020-09-20 17:10:21 +0200

    resource.ts

---
+++ web/src/app/model/resource.ts
@@ -0,0 +1,10 @@
+export class Resource {
+    Id: string;
+    CloudProvider: string;
+    ResourceType: string;
+    AccountId: string;
+    Name: string;
+    Region: string;
+    CreationDate: Date;
+    Tags: string[];
+}
