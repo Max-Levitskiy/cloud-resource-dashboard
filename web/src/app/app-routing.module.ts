6e1cb204057979ff2af500500bb0cfd9352124d2
Max Levitskiy <max@webtree.org>
2020-09-20 17:14:38 +0200

    Add routing to resource list and default redirect to this page

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListComponent} from './components/resources/list/list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '**', redirectTo: 'resources/list'},
+  {path: 'resources/list', component: ListComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
