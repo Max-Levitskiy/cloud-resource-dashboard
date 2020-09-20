0610c5269ee36aae92b2a1aabc71fc37c46982b3
Max Levitskiy <max@webtree.org>
2020-09-20 17:13:51 +0200

    Added angular material, list.component, status.component into app.module

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -4,15 +4,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ListComponent } from './components/resources/list/list.component';
+import {MatTableModule} from '@angular/material/table';
+import { StatusComponent } from './components/es/status/status.component';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    StatusComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+
+    MatTableModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
