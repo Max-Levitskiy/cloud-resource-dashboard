e34734e104732ad9c52b76b5d81cbe589a79224e
Max Levitskiy <max@webtree.org>
2020-09-23 17:03:29 +0200

    web: fix Dockerfile

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
 export const environment = {
-  production: true
+  production: true,
+  es: {
+    host: 'http://localhost',
+    port: 30920,
+    logLevel: 'info',
+    index: {
+      resource: {
+        name: 'resources'
+      }
+    }
+  },
+  api: {
+    host: 'http://localhost',
+    port: 8080
+  }
 };
