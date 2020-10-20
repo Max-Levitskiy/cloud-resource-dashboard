15878e94c22fb5b49134e26d16a3d8dde520c9a9
Max Levitskiy <max@webtree.org>
2020-10-20 21:10:31 +0200

    web: Advanced search form, review fixes

--- web/src/app/components/resources/search-drawer/search-drawer.component.ts
+++ web/src/app/components/resources/search-drawer/search-drawer.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 import {FormGroupState} from 'ngrx-forms';
 import {Store} from '@ngrx/store';
@@ -8,17 +8,18 @@ import {SearchParams} from '../../../forms/search-drawer-form';
 @Component({
   selector: 'app-search-drawer',
   templateUrl: './search-drawer.component.html',
-  styleUrls: ['./search-drawer.component.scss']
+  styleUrls: ['./search-drawer.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SearchDrawerComponent implements OnInit {
 
   form$: Observable<FormGroupState<SearchParams>>;
 
   constructor(private store: Store<State>) {
-    this.form$ = this.store.select('searchForm');
   }
 
   ngOnInit(): void {
+    this.form$ = this.store.select('searchForm');
   }
 
 }
